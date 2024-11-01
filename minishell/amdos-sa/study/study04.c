0f3416118841eb023b55dfd67762506953311788
Amarildo Dos Santos <amarildodossantosoficial@gmail.com>
2024-11-01 15:41:26 +0100

    study04 continue

--- minishell/amdos-sa/study/study04.c
+++ minishell/amdos-sa/study/study04.c
@@ -13,20 +13,51 @@ int	main(void)
 	while (1)
 	{
 		buff = readline("minishel% ");
-		if (!strcmp(buff, "/usr/bin/clear"))
+		if (!buff)
+			break;
+		if (!strcmp(buff, "clear"))
 		{
 			pid_t idfilho = fork();
 			if (idfilho == 0)
 			{
-				//wait(1);
-				args[0] = buff;
+				args[0] = "/usr/bin/clear";
 				args[1] = NULL;
 				execv(args[0], args);
-				free(buff);
+				perror("execv falhou\n");
+				exit(1);
 			}
-			buff = readline("minishel% ");
+			else if (idfilho > 0)
+				wait(NULL);
+			else
+				perror("Error ao criar o processo filho\n");
+		}
+		else if (!strcmp(buff, "ls"))
+		{
+			pid_t idfilho = fork();
+			if (idfilho == 0)
+			{
+				args[0] = "/usr/bin/ls";
+				args[1] = NULL;
+				execv(args[0], args);
+				perror("execv falhou\n");
+				exit(1);
+			}
+			else if (idfilho > 0)
+				wait(NULL);
+			else
+				perror("Error ao criar o processo filho\n");
+		}
+		else if (!strcmp(buff, "exit"))
+		{
+				//args[0] = "/usr/bin/exit";
+				//args[1] = NULL;
+				//execv(args[0], args);
+				//perror("execv falhou\n");
+				exit(0);
+				free(buff);
 		}
 		else
 			printf("not\n");
+		free(buff);
 	}
 }
