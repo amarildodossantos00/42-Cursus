75a068e14ac666dda198e93ada34605fac64e71e
Pedro Zau <pzau@c1r9s1.42luanda.com>
2024-11-08 13:09:34 +0100

    new changers

---
+++ minishell/pzau/minishell/header/header.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pzau <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 12:04:20 by pzau              #+#    #+#             */
+/*   Updated: 2024/11/08 12:35:21 by pzau             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <signal.h>
+
+# include <readline/readline.h>
+# include <readline/history.h>
+
+# include "others/file.h"
+# include "../libx/libx.h"
+
+typedef struct s_vars
+{
+	char *init;
+}	t_vars;
+
+
+void    inicialize_all(t_vars *vars);
+
+#endif
