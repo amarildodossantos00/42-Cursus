eb8a41dd185f971646e9e8e3a134cd1bc098508a
Celmira Tuiango <ctuiango@c1r9s5.42luanda.com>
2024-11-15 10:03:32 +0100

    echo test

--- minishell/ctuiango/all/src/echo.c
+++ minishell/ctuiango/all/src/echo.c
@@ -1,15 +1,32 @@
 #include "../header/header.h"
 
+/*static void	ignore(char *str)
+{
+	int	i;
+	int	nl;
+	char	*token;
+	
+	i = 0;
+	nl = 1;
+	token = strtok(str, " ");
+	/*while (token && strncmp(token, "-n", 2) == 0)
+	{
+		nl = 0;
+		//token = strtok(NULL, " ");
+	}*/ 
+//}*/
+
 void	echo(t_vars *vars)
 {
 	int	i;
 	int	nl;
-	
+
 	i = 1;
 	nl = 1;
-	while (vars->matrix[i] && ft_strncmp(vars->matrix[i], "-n", 2) == 0)
+	while (vars->matrix[i] && ft_strcmp(vars->matrix[i], "-n") == 0)
 	{
 		nl = 0;
+		//ignore(vars->input);
 		i++;
 	}
 	while (vars->matrix[i] != NULL)
@@ -20,5 +37,5 @@ void	echo(t_vars *vars)
 		i++;
 	}
 	if (nl)
-		printf("\n");	
+		printf("\n");
 }
