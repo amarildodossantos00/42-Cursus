369b00380dabe2f0ecb167844cb4e4f360a98340
Celmira Tuiango <ctuiango@c1r10s2.42luanda.com>
2024-11-18 14:29:14 +0100

    add unset

--- minishell/ctuiango/all.1/header/header.h
+++ minishell/ctuiango/all.1/header/header.h
@@ -16,12 +16,12 @@
 
 # include "../libx/libx.h"
 
-typedef struct	s_env
+typedef struct s_env
 {
-	char	*var;
+	char	*key;
 	char	*value;
-	struct s_env	*next;
-}	t_env;
+	struct s_env *next;
+}t_env;
 
 typedef struct s_vars
 {
@@ -29,7 +29,7 @@ typedef struct s_vars
 	char	*path;
 	char	**variables;
 	char	**matrix;
-	t_env	*env_ref;
+	t_env	*env_list;
 }	t_vars;
 
 void	call_prompt(t_vars *vars);
@@ -41,7 +41,6 @@ void    add_variables(t_vars *vars, char *new_str);
 void    print_variables(t_vars *vars);
 void    liberar(t_vars *vars);
 void    get_path(t_vars *vars);
-int	count_variables(char **environ);
 //pzau
 
 //cc
@@ -49,16 +48,11 @@ void	cd(t_vars *vars);
 void    pwd(t_vars *vars);
 void	ft_exit(t_vars *vars);
 void	echo(t_vars *vars);
-void	remove_deli(char *s);
-void	add_and_update(t_env **env_list, char *var, char *value);
-void	ft_remove(t_env **env_list, char *var);
-void	print_env(t_env *env_list);
-void	ft_export(t_vars *vars, char **args);
-t_env	*creat_node(char *var, char *value);
 //cc
 void    free_env_node(t_env *node);
 t_env   *unset_env(t_env *head, const char *key);
 t_env   *add_env(t_env *head, const char *key, const char *value);
 t_env   *init_env_list(char **envp);
 void    free_matrix(char **matrix);
+
 #endif
---
+++ minishell/ctuiango/all.1/src/ft_unset.c
@@ -0,0 +1,93 @@
+#include "../header/header.h"
+
+void    free_env_node(t_env *node)
+{
+    if (node)
+    {
+        free(node->key);
+        free(node->value);
+        free(node);
+    }
+}
+
+t_env   *unset_env(t_env *head, const char *key)
+{
+    t_env   *current;
+    t_env   *previous;
+
+    current = head;
+    previous = NULL;
+    if (!head || !key)
+    {
+        printf("unset: parametros invalidos\n");
+        return (head);
+    }
+    while (current)
+    {
+        if (strcmp(current->key, key) == 0)
+        {
+            if (previous)
+                previous->next = current->next;
+            else
+                head = current->next;
+            free_env_node(current);
+            return (head);
+        }
+        previous = current;
+        current = current->next;
+    }
+    printf("unset: '%s': variavel nao encontrad\n", key);
+    return (head);
+}
+
+t_env   *add_env(t_env *head, const char *key, const char *value)
+{
+    t_env   *new_node;
+
+    new_node = malloc(sizeof(t_env));
+    if (!new_node)
+        return (head);
+    new_node->key = strdup(key);
+    new_node->value = strdup(value);
+    new_node->next = head;
+    return (new_node);
+}
+
+t_env   *init_env_list(char **envp)
+{
+    t_env   *env_list;
+    int i;
+
+    i = 0;
+    env_list = NULL;
+    while (envp[i])
+    {
+        char    *env_entry;
+        env_entry = strdup(envp[i]);
+        if (!env_entry)
+        {
+            perror("erro ao duplicar variavel de ambiente");
+            exit(EXIT_FAILURE);
+        }
+        char    *key;
+        char    *value;
+        key = strtok(env_entry, "=");
+        value = strtok(NULL, "\0");
+        env_list = add_env(env_list, key, value ? value : "");
+        i++;
+    }
+    return (env_list);
+}
+
+void    free_matrix(char **matrix)
+{
+    int i;
+
+    i = 0;
+    while (matrix && matrix[i])
+    {
+        free(matrix[i]);
+        i++;
+    }
+    free(matrix);
+}
