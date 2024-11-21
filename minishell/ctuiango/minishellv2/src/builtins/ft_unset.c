0e412c88df6b74c74fd0e4a6c373495d69efe705
Celmira Tuiango <ctuiango@c1r10s7.42luanda.com>
2024-11-21 13:25:39 +0100

    add unset & resolve bug

---
+++ minishell/ctuiango/minishellv2/src/builtins/ft_unset.c
@@ -0,0 +1,65 @@
+#include "../../header/header.h"
+
+void    free_env_node(t_env *node)
+{
+    if (node)
+    {
+        free(node->var);
+        free(node->value);
+        free(node);
+    }
+}
+
+t_env   *unset_env(t_env *head, char *var)
+{
+    t_env   *current;
+    t_env   *previous;
+
+    current = head;
+    previous = NULL;
+    if (!head || !var)
+        return (head);
+    while (current)
+    {
+        if (strcmp(current->var, var) == 0)
+        {
+            if (previous)
+                previous->next = current->next;
+            else
+                head = current->next;
+            free_env_node(current);
+            return (head);
+        }
+        previous = current;
+        current = current->next;
+    }
+    return (head);
+}
+
+t_env	*ft_unset(t_env *head, char **vars)
+{
+	int	i;
+	
+	i = 0;
+	if (!head || !vars)
+		return (head);
+	while (vars[i])
+	{
+		head = unset_env(head, vars[i]);
+		i++;
+	}
+	return (head);
+}
+
+/*void    free_matrix(char **matrix)
+{
+    int i;
+
+    i = 0;
+    while (matrix && matrix[i])
+    {
+        free(matrix[i]);
+        i++;
+    }
+    free(matrix);
+}*/
---
+++ minishell/ctuiango/minishellv2/src/filev3.c
@@ -0,0 +1,79 @@
+#include "../header/header.h"
+
+static int  cheack_build_builtins(t_vars *vars)
+{
+    if (ft_strncmp(vars->input, "echo", 4) == 0)
+        return (1);
+    if (ft_strncmp(vars->input, "cd", 2) == 0)
+        return (1);
+    if (ft_strncmp(vars->input, "pwd", 3) == 0)
+        return (1);
+    if (ft_strncmp(vars->input, "export", 6) == 0)
+        return (1);
+    if (ft_strncmp(vars->input, "unset", 5) == 0)
+        return (1);
+    if (ft_strncmp(vars->input, "env", 3) == 0)
+        return (1);
+    if (ft_strncmp(vars->input, "exit", 4) == 0)
+        return (1);
+    return (0);
+}
+
+void    build_builtins(t_vars *vars)
+{
+	vars->matrix = ft_split_args(vars->input);
+	if (ft_strncmp(vars->input, "echo", 4) == 0 &&
+		(vars->input[4] == '\0' || vars->input[4] == ' '))
+		echo(vars);
+	if (ft_strncmp(vars->input, "cd", 2) == 0 &&
+		(vars->input[2] == '\0' || vars->input[2] == ' '))
+		cd(vars);
+	if (ft_strncmp(vars->input, "pwd", 3) == 0 &&
+		(vars->input[3] == '\0' || vars->input[3] == ' '))
+		pwd(vars);
+	if (ft_strncmp(vars->input, "export", 6) == 0 &&
+		(vars->input[6] == '\0' || vars->input[6] == ' '))
+		ft_export(vars, vars->matrix);
+	if (ft_strncmp(vars->input, "unset", 5) == 0 &&
+		(vars->input[5] == '\0' || vars->input[5] == ' '))
+		ft_unset(vars->env_ref, &vars->matrix[1]);
+	if (ft_strncmp(vars->input, "env", 3) == 0 &&
+		(vars->input[3] == '\0' || vars->input[3] == ' '))
+		env(vars);
+	if (ft_strncmp(vars->input, "exit", 4) == 0 &&
+		(vars->input[4] == '\0' || vars->input[4] == ' '))
+		ft_exit(vars);
+}
+
+void    all_commands(t_vars *vars)
+{
+    pid_t		pid;
+	size_t		j;
+
+    vars->cargs = count_args(vars->input);
+	vars->args = create_args(vars->input);
+    pid = fork();
+    j = 0;
+    if (pid < 0)
+        perror("Error\n");
+    else if (pid == 0)
+    {
+        signal(SIGINT, SIG_DFL);
+        execute_path(vars);
+        printf("%s: command not found\n", vars->input);
+        exit(EXIT_FAILURE);
+    }
+    else
+    wait(NULL);
+    while (j++ < vars->cargs)
+        free(vars->args[j]);
+    free(vars->args);
+}
+
+void    only_comands(t_vars *vars)
+{
+    if (cheack_build_builtins(vars))
+        build_builtins(vars);
+    else
+        all_commands(vars);
+}
