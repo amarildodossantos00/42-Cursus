804d8b4b6dfe9b84fffe7ef4ac05d67cc8146ac4
Pedro Zau <pzau@c1r10s4.42luanda.com>
2025-01-09 15:08:12 +0100

    amarildo checks this segfault

--- minishell/pzau/official/src/expand_exit_status.c
+++ minishell/pzau/official/src/expand_exit_status.c
@@ -1,5 +1,33 @@
 #include "../header/header.h"
 
+static int		check_on(t_vars *vars)
+{
+	if (ft_strncmp(vars->input, "expr", 4) == 0)
+		return (1);
+	return (0);
+}
+
+static int	count_dollar(t_vars *vars)
+{
+	int		i;
+	int		count;
+
+	i = 0;
+	count = 0;
+	while (vars->input[i])
+	{
+		// Verifica se há "$?" sem exigir espaço após
+		if (vars->input[i] == '$' && vars->input[i + 1] == '?')
+		{
+			count++;
+			i += 2; // Pula os caracteres '$' e '?'
+		}
+		else
+			i++;
+	}
+	return (count > 0 ? count : 1);
+}
+
 void	execute_command(t_vars *vars, char *command)
 {
 	vars->exit_status = 0;
@@ -24,7 +52,7 @@ void	expand_exit_status(t_vars *vars)
 	char	*exit;
 
 	pos = ft_strstr(vars->input, "$?");
-	add_and_update(&vars->env_ref, "?", "0", 0);
+	//add_and_update(&vars->env_ref, "?", "0", 0);
 	if (!pos)
 		return ;
 	vars->exit_status %= 256;
@@ -33,7 +61,7 @@ void	expand_exit_status(t_vars *vars)
 	exit = ft_itoa(vars->exit_status);
 	if (!exit)
 		return ;
-	add_and_update(&vars->env_ref, "?", exit, 0);
+	//add_and_update(&vars->env_ref, "?", exit, 0);
 	new_input = malloc(ft_strlen(vars->input) - 2 + ft_strlen(exit) + 1);
 	if (!new_input)
 	{
@@ -42,8 +70,10 @@ void	expand_exit_status(t_vars *vars)
 	}
 	ft_strncpy(new_input, vars->input, pos - vars->input);
 	new_input[pos - vars->input] = '\0';
-	ft_strcat(new_input, exit);
-	ft_strcat(new_input, pos + 2);
+	new_input = ft_strcat(new_input, exit);
+	new_input = ft_strcat(new_input, pos + 2);
+	//if (check_on(vars))
+	//	new_input = ft_itoa(ft_atoi(exit) * count_dollar(vars));
 	free(exit);
 	free(vars->input);
 	vars->input = new_input;
