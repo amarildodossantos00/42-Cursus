4fd735c2dfcca101c0810159b70be219e0bfbe00
amarildodossantos00 <amarildodossantosoficial@gmail.com>
2025-01-27 14:10:28 +0100

    New project - Cub3D

---
+++ Cub3D/include/cubgame.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cubgame.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amdos-sa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 13:57:02 by amdos-sa          #+#    #+#             */
+/*   Updated: 2025/01/27 14:06:39 by amdos-sa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUBGAME_H
+# define CUBGAME_H
+# include "../minilibx-linux/mlx.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <time.h>
+# include <math.h>
+
+#endif
